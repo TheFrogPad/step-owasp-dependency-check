58c5b0ddf65d0134380b38f285a23d0154fe7d94
TheFrogPad <craig.perez@oracle.com>
2017-11-06 16:50:12 -0800

    Check for proxy setting and supply report output information

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2017, Oracle and/or its affiliates. All rights reserved.
 
-echo "$(date +%H:%M:%S): Running the OWASP dependency-check Wercker Step"
+echo "$(date +%H:%M:%S): Running the OWASP dependency-check wercker Step"
 
 #
 # Check for required dependencies
@@ -24,13 +24,25 @@ hash unzip 2>/dev/null || { echo "$(date +%H:%M:%S): unzip is required, install 
 echo "$(date +%H:%M:%S): OWASP dependency-check requirements complete"
 
 #
+# Proxy information when available [IFF proxy server then include proxy port]
+#
+if [[ -z "$HTTPS_PROXY_SERVER" ]]; then
+  CURL_PROXY=""
+  CHECK_PROXY=""
+  echo "$(date +%H:%M:%S): A proxy will not be used for dependency-check"
+else
+  CURL_PROXY="--proxy $HTTPS_PROXY_SERVER:$HTTPS_PROXY_PORT"
+  CHECK_PROXY="--proxyserver $HTTPS_PROXY_SERVER --proxyport $HTTPS_PROXY_PORT"
+fi
+
+#
 # Download OWASP dependency-check when not already available
 #
 
 if [ ! -d "$WERCKER_CACHE_DIR/owasp" ]; then
   mkdir $WERCKER_CACHE_DIR/owasp
   echo "$(date +%H:%M:%S): Downloading OWASP dependency-check"
-  curl -O -L https://dl.bintray.com/jeremy-long/owasp/dependency-check-3.0.1-release.zip
+  curl $CURL_PROXY -O -L https://dl.bintray.com/jeremy-long/owasp/dependency-check-3.0.1-release.zip
 
   echo "$(date +%H:%M:%S): Extracting OWASP dependency-check"
   unzip -q dependency-check-3.0.1-release.zip -d $WERCKER_CACHE_DIR/owasp
@@ -39,27 +51,23 @@ if [ ! -d "$WERCKER_CACHE_DIR/owasp" ]; then
 else
   if [ ! -x "$WERCKER_CACHE_DIR/owasp/dependency-check/bin/dependency-check.sh" ] ; then
       echo "$(date +%H:%M:%S): ERROR: dependency-check was not installed properly"
+      rm -rf $WERCKER_CACHE_DIR/owasp
       exit 1
   fi
   echo "$(date +%H:%M:%S): OWASP dependency-check already present"
 fi
 
+
+
 #
 # Run OWASP dependency-check
 #
-CHECK_CMD="$WERCKER_CACHE_DIR/owasp/dependency-check/bin/dependency-check.sh --project $WERCKER_OWASP_DEPENDENCY_CHECK_PROJECT --scan $WERCKER_OWASP_DEPENDENCY_CHECK_SCAN --out $WERCKER_OWASP_DEPENDENCY_CHECK_OUT --format $WERCKER_OWASP_DEPENDENCY_CHECK_FORMAT --failOnCVSS $WERCKER_OWASP_DEPENDENCY_CHECK_FAIL_ON_CVSS --data $WERCKER_OWASP_DEPENDENCY_CHECK_DATA"
+CHECK_CMD="$WERCKER_CACHE_DIR/owasp/dependency-check/bin/dependency-check.sh $CHECK_PROXY --project $WERCKER_OWASP_DEPENDENCY_CHECK_PROJECT --scan $WERCKER_OWASP_DEPENDENCY_CHECK_SCAN --out $WERCKER_OWASP_DEPENDENCY_CHECK_OUT --format $WERCKER_OWASP_DEPENDENCY_CHECK_FORMAT --failOnCVSS $WERCKER_OWASP_DEPENDENCY_CHECK_FAIL_ON_CVSS --data $WERCKER_OWASP_DEPENDENCY_CHECK_DATA"
 echo "$(date +%H:%M:%S): Running OWASP dependency-check with command:"
 echo "% $CHECK_CMD"
 $CHECK_CMD
 
 #
-# Save OWASP dependency-check reports
+# Done OWASP dependency-check reports
 #
-echo "$(date +%H:%M:%S): Save OWASP dependency-check reports to $WERCKER_CACHE_DIR"
-echo "% cp -fv $WERCKER_OWASP_DEPENDENCY_CHECK_OUT/dependency-check-*.* $WERCKER_CACHE_DIR"
-cp -fv $WERCKER_OWASP_DEPENDENCY_CHECK_OUT/dependency-check-*.* $WERCKER_CACHE_DIR
-
-#
-# Exit without error so pipeline completes
-#
-exit 0
+echo "$(date +%H:%M:%S): Saved OWASP dependency-check reports to $WERCKER_OWASP_DEPENDENCY_CHECK_OUT"
